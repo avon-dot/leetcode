fed7ed8664c541176ad8e1c50554c4276a739a01
avon-dot <avon_yfmao@126.com>
2023-03-09 18:15:32 +0800

    update.git ignore

---
+++ src/leetcode/editor/cn/[239]滑动窗口最大值.java
@@ -0,0 +1,40 @@
+package leetcode.editor.cn;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution239 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (k == 1){
+            return nums;
+        }
+
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] != o2[0] ? o2[0] - o1[0] : o2[1] - o1[1];
+            }
+        });
+
+        int n = nums.length;
+        for (int i = 0; i < k; i++) {
+            priorityQueue.offer(new int[]{nums[i],i});
+        }
+        int[] ans = new int[nums.length - k + 1];
+        ans[0] = priorityQueue.peek()[0];
+
+        for (int left = 0; left < n - k; left++) {
+            priorityQueue.offer(new int[]{nums[left + k],left + k});
+
+            while (priorityQueue.peek()[1] <= left){
+                priorityQueue.poll();
+            }
+
+            ans[left+1] = priorityQueue.peek()[0];
+        }
+
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
